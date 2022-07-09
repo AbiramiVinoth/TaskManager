5f8aad0e709ff645563a66d9197a4efdaeae4610
Abirami <abisubu14@gmail.com>
2022-07-09 22:07:48 +0530

    db connection added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,93 @@
+require("../db/mongoose");
 const express = require('express');
 const app = express();
 const hbs = require("hbs");
 const path = require("path");
+const bodyParser = require('body-parser');
 
 const publicDirPath = path.join(__dirname, "../public");
 const viewPath = path.join(__dirname, "../templates/views");
 const partialPath = path.join(__dirname, "../templates/partials");
 
+const Users = require('../models/users');
+const Tasks = require('../models/tasks');
+
+
 app.use(express.static(publicDirPath));
 app.set("view engine", "hbs");
 app.set("views", viewPath);
 hbs.registerPartials(partialPath);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//CRUD operation for user table
+app.get('/api/users', function (req, res) {
+  Users.find().then(function(users) {
+    res.status(200).send(users);
+  }).catch(function(error) {
+    res.status(500).send('Internal Server Error');
+  })
+})
+
+app.post('/api/users', function (req, res) {
+  var user = new Users(req.body);
+  user.save().then(function(user) {
+    res.status(201).send('Users saved successfully');
+  }).catch(function(error) {
+    console.log(error);
+    res.status(500).send('Internal Server Error');
+  })
+})
+
+app.put('/api/users/:id', function (req, res) {
+  console.log("request param", req.params.id);
+  console.log("request body", req.body);
+  Users.findByIdAndUpdate(req.params.id, req.body).then(function(user) {
+    res.status(201).send('Users updated successfully');
+  }).catch(function(error) {
+    res.status(500).send('Internal Server Error');
+  })
+})
+
+app.delete('/api/users/:id', function (req, res) {
+  Users.findByIdAndDelete(req.params.id).then(function(user) {
+    res.status(200).send('Users deleted successfully');
+  }).catch(function(error) {
+    console.log(error);
+    res.status(500).send('Internal Server Error');
+  })
+})
+
+// CRUD operation for task table
+app.get('/api/tasks', function (req, res) {
+  Tasks.find().then(function(tasks) {
+    res.status(200).send(tasks);
+  }).catch(function(error) {
+    res.status(500).send('Internal Server Error');
+  })
+})
+
+app.post('/api/tasks', function (req, res) {
+  var task = new Tasks(req.body);
+  task.save().then(function(task) {
+    res.status(201).send('Users saved successfully');
+  }).catch(function(error) {
+    console.log(error);
+    res.status(500).send('Internal Server Error');
+  })
+})
+  
+
+app.post('/api/users', function (req, res) {
+  var task = new Users(req.body);
+  task.save().then(function(task) {
+    res.status(201).send('Users saved successfully');
+  }).catch(function(error) {
+    console.log(error);
+    res.status(500).send('Internal Server Error');
+  })
+})
 
 app.get('/users/add', function (req, res) {
   res.render('addUser');
